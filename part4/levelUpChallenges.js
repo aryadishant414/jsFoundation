d61fbcc95a5de96201397f0aca38025cb4d0df45
dishant <aryadishant414@gmail.com>
2025-02-10 09:08:58 +0530

    or-in loop completed

--- part4/levelUpChallenges.js
+++ part4/levelUpChallenges.js
@@ -13,8 +13,6 @@
 // }
 // console.log("INSIDE SELECTED TEAs array : ", selectedTeas);
 
-
-
 /*
 2. Write a `for` loop that loops through the array `["London", "New York", "Paris", "Berlin"]` and skips `"Paris"`.
     Store the other cities in a new array named `visitedCities`. 
@@ -30,12 +28,16 @@
 // }
 // console.log("Inside visited cities array : ", visitedCities);
 
-
-
 /* 
 3. Use a `for-of` loop to iterate through the array `[1, 2, 3, 4, 5]` and stop when the number `4` is found.
     Store the numbers before `4` in an array named `smallNumbers`.
+
+    // SYNTAX
+// for (const element of object) {
+    
+// }
 */
+
 // let numbers = [1, 2, 3, 4, 5];
 // let smallNumbers = [];
 
@@ -46,8 +48,6 @@
 //     smallNumbers.push(eachElement);
 // }
 // console.log("inside smallNumbers array : ", smallNumbers);
-
-
 
 /*
 4. Use a `for-of` loop to iterate through the array `["chai", "green tea", "herbal tea", "black tea"]` and skip `"herbal tea"`. 
@@ -63,3 +63,71 @@
 //     preferredTeas.push(eachTea);
 // }
 // console.log(preferredTeas);
+
+/*
+5. Use a `for-in` loop to loop through an object containing city populations.
+    Stop the loop when the population of `"Berlin"` is found and store all previous cities populations in a new object named `cityPopulations`.
+
+    let citiesPopulation = {
+    "London": 8900000,
+    "New York": 8400000,
+    "Paris": 2200000,
+    "Berlin": 3500000
+    };
+
+    // SYNTAX
+    for (const key in object) {
+
+    }
+*/
+
+// let citiesPopulation = {
+//     "London": 8900000,
+//     "New York": 8400000,
+//     "Paris": 2200000,
+//     "Berlin": 3500000
+//     };
+
+// let cityNewPopulations = {};
+
+// for (const eachCity in citiesPopulation) {
+//     if(eachCity === "Berlin") {
+//         break;
+//     }
+//     cityNewPopulations[eachCity] = citiesPopulation[eachCity];
+// }
+// console.log("Inside New City Population Object : ", cityNewPopulations);
+
+/*
+6. Use a `for-in` loop to loop through an object containing city populations.
+    Skip any city with a population below 3 million and store the rest in a new object named `largeCities`.
+
+let worldCities = {
+    "Sydney": 5000000,
+    "Tokyo": 9000000,
+    "Berlin": 3500000,
+    "Paris": 2200000
+}
+*/
+
+let worldCities = {
+  Sydney: 5000000,
+  Tokyo: 9000000,
+  Berlin: 3500000,
+  Paris: 2200000,
+};
+
+let largeCities = {};
+
+for (const eachKey in worldCities) {
+    if(worldCities[eachKey] < 3000000) {
+        continue;
+    }
+    largeCities[eachKey] = worldCities[eachKey];
+    // console.log(eachKey);
+    
+}
+console.log("Inside largeCities object : ",largeCities);
+
+
+
