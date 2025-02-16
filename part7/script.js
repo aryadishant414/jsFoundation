0500c456e107ba7b43e9b92358068f96909f95ba
dishant <aryadishant414@gmail.com>
2025-02-16 06:18:05 +0530

    DOM, BOM done

---
+++ part7/script.js
@@ -0,0 +1,18 @@
+console.log("Hello from script");
+// console.log(window);
+// console.log(window.innerHeight);
+// console.log(window.innerWidth);
+
+// console.log(window.outerWidth);  // yeh hamare window ki width hai mtlb ki browser ki actual width hai jo ki change nhi hoti
+
+
+console.log(window.location);
+console.log(location);
+
+console.log(window.navigator);
+console.log(navigator);
+
+
+
+
+
