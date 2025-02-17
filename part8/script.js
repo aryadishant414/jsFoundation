ab19932058a4ed099f918504bd25dcb5d4eabd6f
dishant <aryadishant414@gmail.com>
2025-02-17 19:30:00 +0530

    remaining DOM challenges done

--- part8/script.js
+++ part8/script.js
@@ -82,3 +82,94 @@ document.getElementById("removeLastTask").addEventListener("click", function() {
     
     taskList.lastElementChild.remove();
 })
+
+
+
+// Example 6
+document.getElementById("clickMeButton").addEventListener("click", function() {
+    alert("Clicked");
+})
+
+// document.getElementById("clickMeButton").addEventListener("dblclick", function() {
+//     alert("Clicked");
+// })
+
+// document.getElementById("clickMeButton").addEventListener("mouseover", function() {
+//     alert("Clicked");
+// })
+
+// document.getElementById("clickMeButton").addEventListener("mouseenter", function() {
+//     alert("Clicked");
+// })
+
+// document.getElementById("clickMeButton").addEventListener("mouseleave", function() {
+//     alert("Clicked");
+// })
+
+
+
+// Example 7
+let elee = document.getElementById("teaList");
+console.log(elee);
+
+document.getElementById("teaList").addEventListener("click", function(event) {
+    let clickedTeaElement = event.target
+    // console.log(clickedTeaElement);
+    // console.log(event.target.matches(".teaItem"));
+    
+
+
+    let clickedTea = clickedTeaElement.textContent 
+    // console.log(clickedTea);
+
+    // for safety reasons we are checking this condition because in future there might be some other teas that contains different classes
+    if(clickedTeaElement && clickedTeaElement.matches(".teaItem")) {
+        alert(`${clickedTea} is clicked`)
+    }
+
+})
+
+
+
+
+
+// Example 8
+document.getElementById("feedbackForm").addEventListener("submit", function(event) {
+    event.preventDefault();
+    // console.log(event);
+
+    let ourInput = document.getElementById("feedbackInput");
+    // console.log(ourInput);
+    // console.log(ourInput.value);
+
+
+    document.getElementById("feedbackDisplay").textContent = `Feedback is : ${ourInput.value}`;
+    
+
+    
+
+    // alert("form submitted");
+})
+
+// Example 9
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("domStatus").textContent = "DOM Loaded Successfully";
+})
+
+
+
+// Example 10
+document.getElementById("toggleHighlight").addEventListener("click", function() {
+
+    // console.log(document.getElementById("descriptionText").classList)
+    // document.getElementById("descriptionText").classList.add("highlight")
+    document.getElementById("descriptionText").classList.toggle("highlight")
+
+    // below is my hit and trial
+    // let check = document.getElementById("descriptionText").className;
+    // console.log(check);
+
+    // NOTE : toggle means : ex : dark and light theme ka jo toggle button hota hai uska kya work hota hai ki agr dark theme hai too usko light krdo and light theme hai too usko dark krdo. IN SIMILAR FASHION agr yaha `class` `highlight` lagi hue hai too usko hatado and agr nhi hai too lagado
+    
+    
+})
